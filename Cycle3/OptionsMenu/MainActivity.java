72c0bbaca4fc7d129b4642b1f3b4250a19a8a5d7
Ansha-Uthaman <chn24mc049@ceconline.edu>
2025-10-12 13:29:15 +0530

    Update MainActivity.java

--- Cycle3/OptionsMenu/MainActivity.java
+++ Cycle3/OptionsMenu/MainActivity.java
@@ -1,33 +1,48 @@
-package com.example.explicitintents;
+package com.example.optionsmenu;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
-
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button navigateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Find the button from the layout file by its ID.
-        navigateButton = findViewById(R.id.navigateButton);
-// Set an OnClickListener on the button to handle clicks.
-        navigateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-// An explicit intent is used to launch a specific component (Activity).
-// It requires two arguments: the current context and the target class.
-                Intent explicitIntent = new Intent(MainActivity.this, SecondActivity.class);
-                // Use startActivity() to execute the intent and launch the new activity.
-                startActivity(explicitIntent);
-            }
-        });
+    }
+    // This method is responsible for creating the options menu.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu XML file into the Menu object.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    // This method is called every time a menu item is selected.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+// Get the ID of the selected menu item.
+        int id = item.getItemId();
+// Use a conditional statement to determine which activity to launch.
+        if (id == R.id.action_activity_one) {
+// Create an Intent to start ActivityOne.
+            Intent intent = new Intent(MainActivity.this, activityOne.class);
+            startActivity(intent);
+            Toast.makeText(this, "Navigating to Activity One", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_activity_two) {
+// Create an Intent to start ActivityTwo.
+            Intent intent = new Intent(MainActivity.this, activityTwo.class);
+            startActivity(intent);
+            Toast.makeText(this, "Navigating to Activity Two", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+// Pass the selection to the superclass for default handling if no matches are found.
+        return super.onOptionsItemSelected(item);
     }
 }
